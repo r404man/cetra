d8e7b1ca7530a7b28b7e935ea32fb2d09d99fe94
Tyrtychko Roman <tyrtychko.r404man@gmail.com>
2022-11-07 12:49:46 +0600

    fix

--- src/components/header/header-profile/HeaderProfile.tsx
+++ src/components/header/header-profile/HeaderProfile.tsx
@@ -9,7 +9,12 @@ function loggedIn() {
 		<>
 			<div className="profile-name">AVsd...F12K</div>
 			<div className="profile-icon">
-				<svg width="8" height="7" viewBox="0 0 8 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<svg
+					width="8"
+					height="7"
+					viewBox="0 0 8 7"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg">
 					<path
 						d="M3.13397 6.5C3.51887 7.16667 4.48113 7.16667 4.86603 6.5L7.4641 2C7.849 1.33334 7.36788 0.500002 6.59808 0.500002L1.40193 0.500001C0.632126 0.500001 0.151 1.33333 0.5359 2L3.13397 6.5Z"
 						fill="#1F2040"
